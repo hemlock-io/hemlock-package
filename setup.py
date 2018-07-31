25bc3f178567d8877ca16c7896371b89fd1b7738
hemlock-io <41923872+hemlock-io@users.noreply.github.com>
2018-07-30 22:33:00 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+ 
+long_description = 'This is the upload manager for the Hemlock Platform'
+ 
+setup(name='hemlock',
+      packages=['hemlock'],
+      version='0.9.1', 
+      description='Upload Manager',
+      long_description=long_description,
+      url='https://github.com/hemlock-io/hemlock-package',
+      author='John Alexander Harris',
+      author_email='john@hemlock.io',
+      license='LGPL')
